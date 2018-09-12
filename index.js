8d2df58d1fb72843bd8381c5a7a56a25d5d41755
朱文波 <472647301@qq.com>
2018-09-12 23:29:45 +0800

    demo

--- index.js
+++ index.js
@@ -1,39 +1,67 @@
 const Koa = require('koa')
-const App = new Koa()
-const Chrome = require('./src/chrome')
+const websockify = require('koa-websocket')
+const app = websockify(new Koa())
+const axios = require('axios')
 
-const puppeteer = require('puppeteer')
-
-let Browse
-let BrowseClass
-
-// 配置参数
-const options = {
-  devtools: true,
-  executablePath: './chrome/chrome',
-  args: ['--no-sandbox', '--disable-setuid-sandbox']
+class FetchData {
+  constructor() {
+    this.timer = null
+  }
+  init(params, callback) {
+    this.timer = setInterval(() => {
+      axios.get(`http://www.coinlim.com/service-business-tradingview/v0.1.0/history?symbol=${params.symbol}&resolution=${params.resolution}&from=${params.from}&to=${params.to}`).then(res => {
+        callback && callback(res.data)
+      })
+    }, 1000)
+  }
+  close() {
+    if (this.timer) {
+      clearInterval(this.timer)
+      this.timer = null
+    }
+  }
 }
 
-App.use(async ctx => {
-  if (Browse) {
-    await BrowseClass.init()
-    await BrowseClass.goto('https://www.baidu.com/')
-    // 可执行关闭 await BrowseClass.close()
-    ctx.body = BrowseClass.id
-  } else {
-    const currentChrome = new Chrome(puppeteer)
-    await currentChrome.init(options)
-    await currentChrome.goto('https://www.baidu.com/')
-    // 可执行关闭 await currentChrome.close()
-    ctx.body = currentChrome.id
+const enterTrader = async ctx => {
+  const fetchData = new FetchData()
+  ctx.websocket.send('Hello World')
+  ctx.websocket.on('message', message => {
+    console.log(' >> Message: ', message)
+    if (typeof message === 'string') {
+      try {
+        const params = JSON.parse(message)
+        if (params && params.symbol && params.resolution) {
+          fetchData.init(params, data => {
+            if (ctx.websocket.readyState === 1) {
+              ctx.websocket.send(JSON.stringify(data))
+            }
+          })
+        }
+      } catch (err) {
+        console.error(' >> Parse Error: ', err)
+      }
+    }
+  })
+  ctx.websocket.on('close', () => {
+    fetchData.close()
+    console.log(' >> WebSocket Close.')
+  })
+}
+
+app.ws.use(async ctx => {
+  const url = ctx.request.url
+  const index = url.indexOf('?')
+  const router = index !== -1 ? url.substr(0, index) : url
+  switch (router) {
+    case '/trader':
+      await enterTrader(ctx)
+      break
+    default:
+      ctx.body = { code: 0, error: '参数错误' }
+      break
   }
 })
 
-App.listen(8088, () => {
-  // 多 Page 的话在这里启动 puppeteer
-  puppeteer.launch(options).then(browse => {
-    Browse = browse
-    BrowseClass = new Chrome(null, Browse)
-  })
+app.listen(666, () => {
   console.log(` >> ${new Date().toJSON()} Start service.`)
 })
