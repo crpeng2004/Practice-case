d9a6bf0637a35b6b37fe6d5c199d41ff05f2a77b
朱文波 <472647301@qq.com>
2018-09-13 13:22:59 +0800

    demo

--- index.js
+++ index.js
@@ -6,17 +6,46 @@ const axios = require('axios')
 class FetchData {
   constructor() {
     this.timer = null
-    this.params = {}
+    this.firstTime = null
+    this.baseUrl = 'http://www.coinlim.com/service-business-tradingview/v0.1.0/history'
   }
   init(params, callback) {
     this.timer = setInterval(() => {
-      if (this.params.from !== params.from && this.params.to !== params.to) {
-        this.params = params
-        axios.get(`http://www.coinlim.com/service-business-tradingview/v0.1.0/history?symbol=${params.symbol}&resolution=${params.resolution}&from=${params.from}&to=${params.to}`).then(res => {
-          callback && callback(res.data)
-        })
-      }
+      const { symbol, resolution } = params
+      const { from, to } = this.period(resolution)
+      axios.get(`${this.baseUrl}?symbol=${symbol}&resolution=${resolution}&from=${from}&to=${to}`).then(res => {
+        callback && callback(res.data)
+      })
     }, 1000)
+  }
+  period(resolution) {
+    const time = parseInt(Date.now() / 1000)
+    if (!this.firstTime) {
+      this.firstTime = true
+      console.log(' >> Start Time: ', time - this.periodLength(resolution, 1400), time)
+      return {
+        from: time - this.periodLength(resolution, 1400),
+        to: time
+      }
+    } else {
+      return {
+        from: time - this.periodLength(resolution, 10),
+        to: time
+      }
+    }
+  }
+  periodLength(resolution, requiredPeriodsCount) {
+    let daysCount = 0
+    if (resolution === 'D' || resolution === '1D') {
+      daysCount = requiredPeriodsCount
+    } else if (resolution === 'M' || resolution === '1M') {
+      daysCount = 31 * requiredPeriodsCount
+    } else if (resolution === 'W' || resolution === '1W') {
+      daysCount = 7 * requiredPeriodsCount
+    } else {
+      daysCount = requiredPeriodsCount * parseInt(resolution) / (24 * 60)
+    }
+    return daysCount * 24 * 60 * 60
   }
   close() {
     if (this.timer) {
@@ -28,22 +57,20 @@ class FetchData {
 
 const enterTrader = async ctx => {
   const fetchData = new FetchData()
-  ctx.websocket.send('Hello World')
+  ctx.websocket.send(JSON.stringify({ type: 'Hello World' }))
   ctx.websocket.on('message', message => {
     console.log(' >> Message: ', message)
-    if (typeof message === 'string') {
-      try {
-        const params = JSON.parse(message)
-        if (params && params.symbol && params.resolution) {
-          fetchData.init(params, data => {
-            if (ctx.websocket.readyState === 1) {
-              ctx.websocket.send(JSON.stringify(data))
-            }
-          })
-        }
-      } catch (err) {
-        console.error(' >> Parse Error: ', err)
+    try {
+      const params = JSON.parse(message)
+      if (params && params.symbol && params.resolution) {
+        fetchData.init(params, data => {
+          if (ctx.websocket.readyState === 1) {
+            ctx.websocket.send(JSON.stringify(data))
+          }
+        })
       }
+    } catch (err) {
+      console.error(' >> Parse Error: ', err)
     }
   })
   ctx.websocket.on('close', () => {
